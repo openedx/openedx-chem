507925d46a22c1a6eb65e9b30e90122371984613
stv <stv@stanford.edu>
2019-05-29 10:43:40 -0700

    Unpin requirements and reorder setup.py
    We'll rely on the application (edx-platform) to provide additional
    constraints.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     version='0.4.0',
     packages=["chem"],
     install_requires=[
-        "pyparsing==2.2.0",
-        "numpy==1.6.2",
-        "scipy==0.14.0",
-        "nltk",
-        "markupsafe",  # Should be replaced by other utilities. See LEARNER-5853 for more details.
+        'markupsafe',  # Should be replaced by other utilities. See LEARNER-5853 for more details.
+        'nltk',
+        'numpy',
+        'pyparsing',
+        'scipy',
     ],
     test_suite='chem.tests',
     tests_require=[
