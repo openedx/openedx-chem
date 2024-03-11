269183926d7cf96c93c100af85b03170bc86634e
Chintan Joshi <chintanjoshi1995@gmail.com>
2024-03-11 11:28:22 +0300

    chore: add python 3.12 in setup.py file

--- setup.py
+++ setup.py
@@ -67,5 +67,6 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.12',
     ],
 )
