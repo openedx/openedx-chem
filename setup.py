044b0d5d9440f6b5ff45882d1a3e1ced3c16cbe0
Muhammad Tayyab Tahir Qureshi <tayyab.tahir@arbisoft.com>
2025-04-22 12:09:10 +0500

    chore: Add python 3.12 support
    - drop python 3.8 support
    - upgrade requirements

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         'Topic :: Scientific/Engineering :: Chemistry',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
